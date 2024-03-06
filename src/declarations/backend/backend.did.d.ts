833355ddf305c39e2c0be24bb29489b26c92459e
shubhamQB <shubham.gupta@quadbtech.com>
2024-03-06 23:17:58 +0530

    update

--- src/declarations/backend/backend.did.d.ts
+++ src/declarations/backend/backend.did.d.ts
@@ -1,6 +1,5 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
-import type { IDL } from '@dfinity/candid';
 
 export interface Address {
   'id' : string,
@@ -407,5 +406,3 @@ export interface _SERVICE {
     Result
   >,
 }
-export declare const idlFactory: IDL.InterfaceFactory;
-export declare const init: ({ IDL }: { IDL: IDL }) => IDL.Type[];
